d0acf502719f9f516a80171916b01ef6b239583f
Chase Robinson <srobinson6@mocs.flsouthern.edu>
2023-03-01 10:03:13 -0500

    fixed basic spin

--- Roulette.py
+++ Roulette.py
@@ -3,26 +3,36 @@ import numpy as np
 import random
 import pdb
 
+class Spot:
+    def __init__(self, number, color):
+        self.number = number
+        self.color = color
 
-
+    def get_num(self):
+        return self.number
+    
+    def get_color(self):
+        return self.color
+    
+    def get_numColor(self):
+        return str(self.number) + ' ' + str(self.color)
 
 class Roulette:
-    # list of lists
-        # inner list is the number followed by color
-        # 'number', [0 = red, 1 = black, 2 = green]
-    wheel = np.array([
-            ('00', 2), ('27', 0), ('10', 1), ('25', 0), ('29', 1), ('12', 0), 
-            ('8', 1), ('19', 0), ('31', 1), ('18', 0), ('6', 1), ('21', 0), ('33', 1), 
-            ('16', 0), ('4',1 ), ('23', 0), ('35', 1), ('14', 0), ('2', 1), ('0', 2),
-            ('28', 1), ('9', 0), ('26', 1), ('30', 0), ('11', 1), ('7', 0), ('20', 1),
-            ('32', 0), ('17',1 ), ('5', 0), ('22', 1), ('34', 0), ('15', 1), ('3', 0),
-            ('24', 1), ('36', 0), ('13', 1), ('1', 0)
-        ])
+    red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+    black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
     
     def __init__(self, seed=0):
         self.seed = seed
-        
+        np.random.seed(seed)
+        self.setup_wheel()
 
+    def setup_wheel(self):
+        temp = []
+        nums = ['00', '27', '10', '25', '29', '12', '8', '19', '31', '18', '6', '21', '33', '16', '4', '23', '35', '14', '2', '0', '28', '9', '26', '30', '11', '7', '20', '32', '17', '5', '22', '34', '15', '3', '24', '36', '13', '1']
+        colors = [2, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+        for i in range(len(nums)):
+            temp.append(Spot(nums[i], colors[i]))
+        self.wheel = temp
 
     def spin(self):
         num = self._get_spin_number()
@@ -36,7 +46,11 @@ class Roulette:
 
     def _get_spin_number(self):
         return np.random.choice(self.wheel)
+    
+    def _color_to_string(self, num):
+        colors = ['red', 'black', 'green']
+        return colors[num]
 
 
 r = Roulette()
-print(r.spin())
+print(r.spin().get_numColor())
